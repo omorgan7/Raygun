b8de696385644f1e9fc80b77e1d736e9c6861327
Owen Morgan <o.morgan@bath.ac.uk>
2017-02-15 11:14:40 +0000

    Initial Commit

---
+++ Raygun/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Raygun
+//
+//  Created by Owen Morgan on 15/02/2017.
+//  Copyright © 2017 Owen Morgan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
