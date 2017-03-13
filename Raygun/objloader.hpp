b8052d5f592746e0114935978fca11fd75d7ed32
Owen Morgan <o.morgan@bath.ac.uk>
2017-03-13 11:26:04 +0000

    Added mesh loader files.

---
+++ Raygun/objloader.hpp
@@ -0,0 +1,6 @@
+#ifndef OBJLOADER_H
+#define OBJLOADER_H
+
+bool loadSimpleOBJ(const char * path,std::vector<std::vector <float> > & out_vertices,std::vector<unsigned int> & out_vertex_indices);
+
+#endif
